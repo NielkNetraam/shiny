ab181602504a3611e09dc86401440c076466365b
NielkNetraam <maarten.klein@tisit.nl>
2017-01-10 13:09:12 +0100

    Added data directory

--- MUH2016/server.R
+++ MUH2016/server.R
@@ -18,7 +18,7 @@ library(googleway)
 
 #load track data
 names <-c("Rhenen", "2", "3", "4", "5", "6" ,"Kwintelooijen", "8", "9", "10", "11", "Amerongen", "13","14","15","Hoge Ginkel","17","18","19","20","21","22","Leersum","Zeist","25","26", "27","28","29","Austerlitz")
-sectorDaily <- read.csv("./sectorAggregate.csv", sep=";")
+sectorDaily <- read.csv("./Data/sectorAggregate.csv", sep=";")
 sectorDaily <- sectorDaily %>% 
     filter(dmy(date) <= ymd("2016-12-31")) %>%
     mutate(date=dmy(date),
@@ -26,7 +26,7 @@ sectorDaily <- sectorDaily %>%
            name = names[id])
 
 #load tracks
-json_file <- "./track.json"
+json_file <- "./Data/track.json"
 tracks <- fromJSON(file=json_file)
 tracksdf <- do.call("rbind.fill", lapply(tracks$data, as.data.frame))
 tracksdf <- tracksdf %>% mutate(name = as.character(name), color = as.character(color), polyline = as.character(polyline)) %>% filter(id != 8, type == "permanent") 
